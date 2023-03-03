b4c53b8685a0a4b48faaa33732163b6ac0ee8669
GayanPradeep97 <gayanpradeep0212@gmail.com>
2023-03-03 03:39:48 -0800

    update

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <Header classname={classes}></Header>
+    );
+}
+
+export default Header;
